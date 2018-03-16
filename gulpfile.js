e14c26c0f6666871f350f0f76dd5631bb64530db
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2018-03-16 21:47:37 +0700

    Add npm, gulp, sass

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/global.scss')
+    .pipe(sass({
+        outputStyle: 'compressed',
+      })
+      .on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*', ['sass']);
+});
+
+gulp.task('watch', ['sass:watch']);
