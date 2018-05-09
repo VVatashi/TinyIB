58e162dd371d95a3ba97b5824b58dee303ba7d67
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2018-05-09 17:50:39 +0700

    Fix posting form

--- ts/modules/DEFix.ts
+++ ts/modules/DEFix.ts
@@ -9,10 +9,29 @@ export default class DEFix extends BaseModule {
 
   onReady() {
     setTimeout(() => {
-      // Reset the posting form size.
+      // Reset the message size.
       const message = qid('message') as HTMLTextAreaElement;
-      message.style.width = '';
-      message.style.height = '';
-    });
+      message.classList.remove('de-textarea');
+
+      // Fix file preview.
+      const preview = qs('.de-file');
+
+      if (preview) {
+        const preview_field = document.createElement('tr');
+        preview_field.classList.add('form__field', 'form__field_fixed-width', 'field');
+
+        const preview_label_wrapper = document.createElement('td');
+        preview_label_wrapper.classList.add('field__label-wrapper');
+        preview_field.appendChild(preview_label_wrapper);
+
+        const preview_input_wrapper = document.createElement('td');
+        preview_input_wrapper.classList.add('field__input-wrapper');
+        preview_input_wrapper.appendChild(preview);
+        preview_field.appendChild(preview_input_wrapper);
+
+        const message_group = qs('.form__group_message');
+        message_group.appendChild(preview_field);
+      }
+    }, 1000);
   }
 }
