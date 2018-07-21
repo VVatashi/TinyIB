415274de35066ac7aba0d05f1460450396865bfa
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2018-07-21 09:06:07 +0700

    Fix tests

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -234,14 +234,14 @@ final class PostTest extends TestCase
         // Try set & get the size of the file attached to the post.
         // Input => expected.
         $tests = [
-            0 => '0 B',
-            100 => '100 B',
-            500 => '500 B',
-            1000 => '1 KB',
-            1234 => '1.23 KB',
-            12345 => '12.34 KB',
-            123456 => '123.46 KB',
-            1234567 => '1.23 MB',
+            0 => '0&nbsp;B',
+            100 => '100&nbsp;B',
+            500 => '500&nbsp;B',
+            1000 => '1&nbsp;KB',
+            1234 => '1.23&nbsp;KB',
+            12345 => '12.34&nbsp;KB',
+            123456 => '123.46&nbsp;KB',
+            1234567 => '1.23&nbsp;MB',
         ];
 
         foreach ($tests as $input => $expected) {
