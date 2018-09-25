4f0ad0011c4d06d2081165db3b2e7d7cf0912aaa
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2018-09-25 11:43:38 +0700

    Add post reference map to the mobile version

--- ts/mobile.ts
+++ ts/mobile.ts
@@ -4,6 +4,7 @@ import PostCorrectTime from './modules/mobile/PostCorrectTime';
 import PostForm from './modules/mobile/PostForm';
 import PostImagePopup from './modules/mobile/PostImagePopup';
 import PostQuote from './modules/mobile/PostQuote';
+import PostReferenceMap from './modules/mobile/PostReferenceMap';
 import ThreadUpdater from './modules/mobile/ThreadUpdater';
 
 declare global {
@@ -20,6 +21,7 @@ moduleManager.addModule('PostCorrectTime', new PostCorrectTime(moduleManager));
 moduleManager.addModule('PostForm', new PostForm(moduleManager));
 moduleManager.addModule('PostImagePopup', new PostImagePopup(moduleManager));
 moduleManager.addModule('PostQuote', new PostQuote(moduleManager));
+moduleManager.addModule('PostReferenceMap', new PostReferenceMap(moduleManager));
 moduleManager.addModule('ThreadUpdater', new ThreadUpdater(moduleManager));
 
 window.tinyib = {};
---
+++ ts/modules/mobile/PostReferenceMap.ts
@@ -0,0 +1,48 @@
+import PostModule from './PostModule';
+import ModuleManager from '../../ModuleManager';
+import { qs, qsa } from '../../utils/DOM';
+
+export default class PostReferenceMap extends PostModule {
+  protected posts: { [id: number]: Element } = {};
+  //protected references: { [id: number]: number[] } = {};
+
+  constructor(manager: ModuleManager) {
+    super(manager);
+  }
+
+  protected processPost(post: Element) {
+    const postId = +post.getAttribute('data-post-id');
+
+    // Store post.
+    this.posts[postId] = post;
+
+    // Get references.
+    const referenceElements = qsa('a[data-target-post-id]', post);
+    const references = referenceElements.map(element => {
+      return +element.getAttribute('data-target-post-id');
+    });
+
+    // Get referenced posts.
+    const referencedPosts = references
+      .map(reference => this.posts[reference])
+      .filter(post => post);
+
+    // Append to posts a reference to this post.
+    referencedPosts.forEach(post => {
+      // Create post footer if it is not exists.
+      let footer = qs('.post__footer', post);
+      if (!footer) {
+        footer = document.createElement('div');
+        footer.classList.add('post__footer');
+        post.appendChild(footer);
+      }
+
+      // Append reference to the footer.
+      const link = document.createElement('a');
+      link.classList.add('post__reference-link');
+      link.href = `#post_${postId}`;
+      link.textContent = `>>${postId}`;
+      footer.appendChild(link);
+    });
+  }
+}
