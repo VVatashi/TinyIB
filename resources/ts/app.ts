b55006be54565e1f9b98672a6c997109ce394176
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2019-05-06 18:49:45 +0700

    Remove legacy settings

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -20,44 +20,6 @@ new PostingForm();
 new PostReferenceMap();
 
 document.addEventListener('DOMContentLoaded', e => {
-  /** @todo Remove later */
-  // Migrate old settings.
-  if (LocalStorage.has('settings')) {
-    const settings = JSON.parse(localStorage.getItem('settings'));
-    LocalStorage.set('filter.remove-hidden-posts', settings.common.removeHiddenPosts);
-    LocalStorage.set('filter.hidden-authors', settings.common.hiddenPosts);
-    LocalStorage.set('post.enable-thread-autoupdate', settings.common.enableThreadAutoupdate);
-    LocalStorage.set('post.scroll-to-new-posts', settings.common.scrollToNewPosts);
-    LocalStorage.set('post.reflink-icon-position', settings.common.showPostReflinkIcon ? 'message' : 'header');
-    LocalStorage.set('image.nsfw', settings.common.nsfw);
-    LocalStorage.set('image.show-video-overlay', settings.common.showVideoOverlay);
-    LocalStorage.set('image.hide-popup-on-outside-click', settings.common.hidePopupOnOutsideClick);
-    LocalStorage.set('image.auto-play', settings.common.autoPlay);
-    LocalStorage.set('link.add-names-to-links', settings.common.addNamesToLinks);
-    LocalStorage.set('link.add-you-to-links', settings.common.addYouToLinks);
-    LocalStorage.set('link.show-post-popups', settings.common.showPostPopups);
-    LocalStorage.set('form.scroll-bottom', settings.form.scrollBottom);
-    LocalStorage.set('form.save-subject', settings.form.saveSubject);
-    LocalStorage.set('form.save-name', settings.form.saveName);
-    LocalStorage.set('form.show-markup', settings.form.showMarkup);
-    LocalStorage.set('form.show-markup-mobile', settings.form.showMarkupMobile);
-    LocalStorage.set('form.save-form-state', settings.form.saveFormState);
-    LocalStorage.set('form.align', settings.form.align);
-    LocalStorage.set('form.preview-align', settings.form.previewAlign);
-    LocalStorage.set('form.replaces', settings.form.replaces);
-    LocalStorage.set('form.insert-tags-in-pairs', settings.form.insertTagsInPairs);
-    LocalStorage.set('time.locale', settings.time.locale);
-    LocalStorage.set('time.locale-custom', settings.time.localeCustom);
-    LocalStorage.set('time.format', settings.time.format);
-    LocalStorage.set('time.format-custom', settings.time.formatCustom);
-    LocalStorage.set('time.zone', settings.time.zone);
-    LocalStorage.set('time.zone-fixed', settings.time.zoneFixed);
-    LocalStorage.set('common.smooth-scroll', settings.common.smoothScroll);
-    LocalStorage.set('common.show-unread-count-in-title', settings.common.showUnreadCountInTitle);
-    LocalStorage.set('common.layout', settings.common.layout);
-    localStorage.removeItem('settings');
-  }
-
   eventBus.emit(Events.Ready);
 
   const posts = DOM.qsa('.post');
